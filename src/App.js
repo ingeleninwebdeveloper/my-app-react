8bbf35ce6ae7ddad55ebed96e9391b9fcbaec123
lenin fernando <ingeleninfernando@gmail.com>
2017-01-25 08:39:10 -0500

    arreglo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      
       <h1> Mi primera aplicacion </h1>
       <p> Esta es mi primera aplicación en React, está padrísimo! </p>
-      </div>
+
 
     );
   }
